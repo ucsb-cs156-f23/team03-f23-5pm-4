248123084334e060eef34183458458223f6447e8
mitali <mitaligaidhani@gmail.com>
2023-11-03 22:48:35 -0700

    mg - added fixtures

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,42 @@
+const menuItemReviewFixtures = {
+    oneReview:
+    [
+        {
+            "id": 1,
+            "itemId": 1,
+            "reviewerEmail": "janedoe@ucsb.edu",
+            "stars": 3,
+            "dateReviewed": "2021-01-02T12:00:00",
+            "comments": "good"
+        }
+    ],
+    threeReviews: 
+    [
+        {
+            "id": 1,
+            "itemId": 1,
+            "reviewerEmail": "janedoe@ucsb.edu",
+            "stars": 3,
+            "dateReviewed": "2021-01-02T12:00:00",
+            "comments": "good"
+        },
+        {
+            "id": 2,
+            "itemId": 2,
+            "reviewerEmail": "someoneelse@ucsb.edu",
+            "stars": 4,
+            "dateReviewed": "2022-01-02T12:00:00",
+            "comments": "okay"
+        },
+        {
+            "id": 3,
+            "itemId": 3,
+            "reviewerEmail": "johndoe@ucsb.edu",
+            "stars": 5,
+            "dateReviewed": "2023-01-02T12:00:00",
+            "comments": "amazing"
+        }
+    ]
+};
+
+export { menuItemReviewFixtures };
