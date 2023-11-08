e0fbf92cf59aa6b497742c3a2151a51395aa271c
Katy Tsao <katy.tsao@tempo.io>
2023-11-07 22:32:47 -0800

    kt - add fixtures for articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "How to Write an Article",
+        "url": "team03.dokku-04.cs.ucsb.edu",
+        "explanation": "how to write a really amazing article",
+        "email": "katytsao@ucsb.edu",
+        "dateAdded": "2023-11-07T14:00:00"
+    },
+    threeArticles: [
+        {
+            "id": 1,
+            "title": "How to Write an Article",
+            "url": "team03.dokku-04.cs.ucsb.edu",
+            "explanation": "how to write a really amazing article",
+            "email": "katytsao@ucsb.edu",
+            "dateAdded": "2023-11-07T14:00:00"
+        },
+        {
+            "id": 2,
+            "title": "Why UCSB is Super Cool",
+            "url": "cs.ucsb.edu",
+            "explanation": "the coolest major at the coolest school",
+            "email": "fake-email@ucsb.edu",
+            "dateAdded": "2020-01-02T12:30:00"
+        },
+        {
+            "id": 3,
+            "title": "Something Related to Politics",
+            "url": "us.gov",
+            "explanation": "this description is super controversial",
+            "email": "real.person@realwebsite.com",
+            "dateAdded": "2022-08-28T17:17:17"
+        }
+    ]
+};
+
+
+export { articlesFixtures };
