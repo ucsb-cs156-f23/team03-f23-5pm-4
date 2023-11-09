4ff9fac67d3600cf5f05f0d8f1c88b568ae915c9
mitali <mitaligaidhani@gmail.com>
2023-11-08 17:21:55 -0800

    mg - corrected paths for index page

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewIndexPage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewIndexPage.js
@@ -7,7 +7,7 @@ export default function MenuItemReviewIndexPage() {
     <BasicLayout>
       <div className="pt-2">
         <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
+        <p><a href="/menuitemreview/create">Create</a></p>
         <p><a href="/placeholder/edit/1">Edit</a></p>
       </div>
     </BasicLayout>
