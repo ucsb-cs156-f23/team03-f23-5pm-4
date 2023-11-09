e89eafa8b6eea15803ecffe392fde7f23d76b96b
Francisco Leyva <leyvajustin@rocketmail.com>
2023-11-09 01:17:38 -0800

    form

---
+++ frontend/src/main/components/HelpRequest/HelpRequestForm.js
@@ -0,0 +1,165 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function HelpRequestForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+    const{
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    
+
+    const navigate = useNavigate();
+
+    // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+    // Note that even this complex regex may still need some tweaks
+
+    // Stryker disable next-line Regex
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    // Stryker disable next-line all
+    const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
+    const email_regex = /[a-z0-9_.]+@[a-z]+\.[a-z]+/i;
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+
+            <Row>
+
+                {initialContents && (
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Form.Label htmlFor="id">Id</Form.Label>
+                            <Form.Control
+                            data-testid="HelpRequestForm-id"
+                            id="id"
+                            type="text"
+                            {...register("id")}
+                            value={initialContents.id}
+                            disabled
+                            />
+                        </Form.Group>
+                    </Col>
+                )}
+
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="requesterEmail">Requester Email</Form.Label>
+                        <Form.Control
+                            data-testid="HelpRequestForm-requesterEmail"
+                            id="requesterEmail"
+                            type="text"
+                            isInvalid={Boolean(errors.requesterEmail)}
+                            {...register("requesterEmail", { required: true, pattern: email_regex})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.requesterEmail && 'Requester email is required'}
+                            {errors.requesterEmail?.type === 'pattern' && 'Requester email must be in the form of an email address'}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="teamId">Team Id</Form.Label>
+                        <Form.Control
+                            data-testid="HelpRequestForm-teamId"
+                            id="teamId"
+                            type="text"
+                            isInvalid={Boolean(errors.teamId)}
+                            {...register("teamId", { required: true})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.teamId && 'teamId is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                    <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="tableOrBreakoutRoom">Table Or Breakout Room</Form.Label>
+                        <Form.Control
+                            data-testid="HelpRequestForm-tableOrBreakoutRoom"
+                            id="tableOrBreakoutRoom"
+                            type="text"
+                            isInvalid={Boolean(errors.tableOrBreakoutRoom)}
+                            {...register("tableOrBreakoutRoom", { required: true})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.tableOrBreakoutRoom && 'tableOrBreakoutRoom is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>   
+                <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="requestTime"> Request Time </Form.Label>
+                        <Form.Control
+                            data-testid="HelpRequestForm-requestTime"
+                            id="requestTime"
+                            type="text"
+                            isInvalid={Boolean(errors.requestTime)}
+                            {...register("requestTime", { required: true, pattern: isodate_regex })}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.requestTime && 'requestTime is required.'}
+                                {errors.requestTime?.type === 'pattern' && 'Request time must be in ISO date format YYYY-mm-ddTHH:MM:SS'}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="explanation">explanation</Form.Label>
+                        <Form.Control
+                            data-testid="HelpRequestForm-explanation"
+                            id="explanation"
+                            type="text"
+                            isInvalid={Boolean(errors.explanation)}
+                            {...register("explanation", { required: true})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.explanation && 'explanation is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="solved">Solved</Form.Label>
+                        <Form.Control
+                            data-testid="HelpRequestForm-solved"
+                            id="solved"
+                            type="text"
+                            isInvalid={Boolean(errors.solved)}
+                            {...register("solved", { required: true})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.solved && 'solved is required.'}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>  
+                    
+                    <Col>
+                        <Button
+                            type="submit"
+                            data-testid="HelpRequestForm-submit"
+                        >
+                            {buttonLabel}
+                        </Button>
+                        <Button
+                            variant="Secondary"
+                            onClick={() => navigate(-1)}
+                            data-testid="HelpRequestForm-cancel"
+                        >
+                            Cancel
+                        </Button>
+                    </Col>
+                </Row>
+                </Form>
+    )
+
+}
+export default HelpRequestForm;
---
+++ frontend/src/tests/components/HelpRequest/HelpRequestForm.test.js
@@ -0,0 +1,130 @@
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import HelpRequestForm from "main/components/HelpRequest/HelpRequestForm";
+import { HelpRequestFixtures } from "fixtures/helpRequestFixtures";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+
+describe("HelpRequestForm tests", () => {
+
+    test("renders correctly", async () => {
+
+        render(
+            <Router  >
+                <HelpRequestForm />
+            </Router>
+        );
+        await screen.findByText(/Create/);
+        await screen.findByText(/Requester Email/);
+    });
+
+
+    test("renders correctly when passing in a help request", async () => {
+
+        render(
+            <Router  >
+                <HelpRequestForm initialContents={HelpRequestFixtures.oneHelpRequest} />
+            </Router>
+        );
+        await screen.findByTestId(/HelpRequestForm-id/);
+        expect(screen.getAllByText(/Id/).length).toBe(2);
+        expect(screen.getByTestId(/HelpRequestForm-id/)).toHaveValue("1");
+         });
+
+
+    test("Correct Error messsages on bad input", async () => {
+
+        render(
+            <Router  >
+                <HelpRequestForm />
+            </Router>
+        );
+        await screen.findByTestId("HelpRequestForm-requestTime");
+        const requesterTime = screen.getByTestId("HelpRequestForm-requestTime");
+        const submitButton = screen.getByTestId("HelpRequestForm-submit");
+        
+        fireEvent.change(requesterTime, { target: { value: 'bad-input' } });
+        fireEvent.click(submitButton);
+        
+        await screen.findByText(/Request time must be in ISO date format YYYY-mm-ddTHH:MM:SS/);
+    });
+
+    test("Correct Error messsages on missing input", async () => {
+
+        render(
+            <Router  >
+                <HelpRequestForm />
+            </Router>
+        );
+        await screen.findByTestId("HelpRequestForm-submit");
+        const submitButton = screen.getByTestId("HelpRequestForm-submit");
+
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Requester email is required/);
+        expect(screen.getByText(/teamId is required./)).toBeInTheDocument();
+        expect(screen.getByText(/tableOrBreakoutRoom is required./)).toBeInTheDocument();
+        expect(screen.getByText(/requestTime is required./)).toBeInTheDocument();
+        expect(screen.getByText(/explanation is required./)).toBeInTheDocument();
+        expect(screen.getByText(/solved is required./)).toBeInTheDocument();
+        
+    });
+
+    test("No Error messsages on good input", async () => {
+
+        const mockSubmitAction = jest.fn();
+
+
+        render(
+            <Router  >
+                <HelpRequestForm submitAction={mockSubmitAction} />
+            </Router>
+        );
+        await screen.findByTestId("HelpRequestForm-requesterEmail");
+        const requesterEmailField = screen.getByTestId("HelpRequestForm-requesterEmail");
+        const teamIdField = screen.getByTestId("HelpRequestForm-teamId");
+        const tableOrBreakoutRoomField = screen.getByTestId("HelpRequestForm-tableOrBreakoutRoom");
+        const requestTimeField = screen.getByTestId("HelpRequestForm-requestTime");
+        const explanationField = screen.getByTestId("HelpRequestForm-explanation");
+        const solvedField = screen.getByTestId("HelpRequestForm-solved");
+        const submitButton = screen.getByTestId("HelpRequestForm-submit");
+        fireEvent.change(requesterEmailField, { target: { value: 'student@ucsb.com' } });
+        fireEvent.change(teamIdField, { target: { value: 'one' } });
+        fireEvent.change(tableOrBreakoutRoomField, { target: { value: 'table' } });
+        fireEvent.change(requestTimeField, { target: { value: '2021-10-01T12:00:00' } });
+        fireEvent.change(explanationField, { target: { value: 'I need help with my homework' } });
+        fireEvent.change(solvedField, { target: { value: 'false' } });
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+        expect(screen.queryByText(/Request time must be in ISO format/)).not.toBeInTheDocument();
+
+    });
+
+
+    test("that navigate(-1) is called when Cancel is clicked", async () => {
+
+        render(
+            <Router  >
+                <HelpRequestForm />
+            </Router>
+        );
+        await screen.findByTestId("HelpRequestForm-cancel");
+        const cancelButton = screen.getByTestId("HelpRequestForm-cancel");
+
+        fireEvent.click(cancelButton);
+
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+
+    });
+
+});
+
+
