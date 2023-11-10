3bcfc05390d37ab998cac6333697f8736b516bc1
Francisco Leyva <leyvajustin@rocketmail.com>
2023-11-09 22:10:05 -0800

    init commit

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
@@ -1,14 +1,47 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestsTable from 'main/components/HelpRequest/HelpRequestTable';
+import { Button } from 'react-bootstrap';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser';
 
 export default function HelpRequestIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+
+  const { data: helprequests, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/helprequests/all"],
+      { method: "GET", url: "/api/helprequests/all" },
+      // Stryker disable next-line all : don't test default value of empty list
+      []
+    );
+
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        return (
+            <Button
+                variant="primary"
+                href="/helprequests/create"
+                style={{ float: "right" }}
+            >
+                Create HelpRequest 
+            </Button>
+        )
+    } 
+  };
+
+// console.log(helprequests)
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/helprequest/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
+        {createButton()}
+        <h1>HelpRequests</h1>
+        <HelpRequestsTable helpRequests = {helprequests} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
