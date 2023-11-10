6662c58caa2f20954b3fbf9056bf30393ef3cb20
mitali <mitaligaidhani@gmail.com>
2023-11-09 17:05:36 -0800

    mg - disable regex

--- frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -18,7 +18,9 @@ function MenuItemReviewForm({ initialContents, submitAction, buttonLabel = "Crea
 
     // Stryker disable next-line Regex
     const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+    // Stryker disable next-line Regex
     const stars_regex = /[1-5]/i; // Accepts from 1-5.  Close enough.
+    // Stryker disable next-line all
     const email_regex = /[a-z0-9_.]+@[a-z]+\.[a-z]+/i;
   
     return (
